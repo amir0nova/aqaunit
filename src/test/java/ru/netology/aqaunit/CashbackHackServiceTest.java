e72e627ae9f5a147e6167b1c6a1eacc8650d882a
Alexandra Mironova <sominka1939@gmail.com>
2020-03-24 01:44:22 +0300

    In file src/test/java/ru/netology/aqaunit/CashbackHackServiceTest.java addad 1 negative, 4 positive methods

---
+++ src/test/java/ru/netology/aqaunit/CashbackHackServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology.aqaunit;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    @Test
+    //отрицательный тест на проверку тестового метода
+    void testNegativeOfAdd200IfAmountLowerThen1000Equals900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 200;
+        assertEquals(expected, actual); // должен упасть!
+    }
+
+    @Test
+    void shouldAdd100IfAmountLowerThen1000Equals900() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAdd0IfAmountIs1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAdd375IfAmountLowerThen2000Equals1625() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1625;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 375;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldAdd0IfAmountIs2000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 2000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+}
