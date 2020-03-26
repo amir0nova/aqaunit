029af11a4b380a0c8d1d998dee47d92d3c4aa056
Alexandra Mironova <sominka1939@gmail.com>
2020-03-26 18:41:48 +0300

    Added ParameterizedTest instead of 4 tests, create file CashbackData.csv

--- src/test/java/ru/netology/aqaunit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/aqaunit/CashbackHackServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.aqaunit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +15,18 @@ class CashbackHackServiceTest {
         assertThrows(IllegalArgumentException.class, () -> cashbackHackService.remain(amount));
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/CashbackData.csv", numLinesToSkip = 1)
+    void shouldCalculatecashbackHackTest(int amount, int expected) {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+
+        int actual = cashbackHackService.remain(amount);
+
+        assertEquals(expected, actual);
+    }
+
+    // Sorry, this is only for me. I'm a kettle!
+    /*@Test
     void shouldAdd100IfAmountLowerThen1000Equals900() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
@@ -38,5 +51,5 @@ class CashbackHackServiceTest {
         int actual = cashbackHackService.remain(amount);
         int expected = 375;
         assertEquals(expected, actual);
-    }
+    }*/
 }
