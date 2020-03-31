04b7206f6332973ffc35139b5a8989abc6f966b3
Alexandra Mironova <sominka1939@gmail.com>
2020-03-31 12:09:11 +0300

    Deleted commented code in CashbackHackServiceTest

--- src/test/java/ru/netology/aqaunit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/aqaunit/CashbackHackServiceTest.java
@@ -24,32 +24,4 @@ class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
-
-    // Sorry, this is only for me. I'm a kettle!
-    /*@Test
-    void shouldAdd100IfAmountLowerThen1000Equals900() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 900;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 100;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldAdd0IfAmountIs1000() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 0;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void calculateShouldAdd375IfAmountEquals1625() {
-        CashbackHackService cashbackHackService = new CashbackHackService();
-        int amount = 1625;
-        int actual = cashbackHackService.remain(amount);
-        int expected = 375;
-        assertEquals(expected, actual);
-    }*/
 }
